9a79b501e54acac17aac5ba57633e0d1cb187120
LukeBMorrow <lukebmorrow@gmail.com>
2018-09-04 16:44:55 -0500

    This is my second commit, just a simple comment.

--- src/com/Luke/Main.java
+++ src/com/Luke/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         for(String i:args)
-        System.out.print(" "+i);
+        System.out.print(" "+i);//I am a comment
     }
 }
